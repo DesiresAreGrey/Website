639b3bbb94d77a17b03a2dadf0d61a28e25ecd2b
DesiresAreGrey <desiresaregrey@gmail.com>
2025-11-09 20:30:11 -0500

    make blast pattern dots slightly smaller

--- docs/misc/apex-weapon-stats/apex-weapon-stats.js
+++ docs/misc/apex-weapon-stats/apex-weapon-stats.js
@@ -704,7 +704,7 @@ function updateWeaponStats(column) {
 
     column.querySelector("#recoil-pattern").drawPattern(column.mode.Firing.Shot.RecoilPattern, [-7.75, -14.75, 7.75, 0.75], true, true, 0.15, 0.1, '#fff', '#666');
 
-    column.querySelector("#blast-pattern").drawPattern(column.mode.Firing.Shot.BlastPattern, undefined, false, false, 1.25);
+    column.querySelector("#blast-pattern").drawPattern(column.mode.Firing.Shot.BlastPattern, undefined, false, false, 1);
 
     // Reload Time
 
