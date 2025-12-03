98ea3ad7612bb142f02908ec230b5e999202c018
DesiresAreGrey <desiresaregrey@gmail.com>
2025-12-03 17:56:37 -0500

    organize utils

--- src/utils.ts
+++ src/utils.ts
@@ -12,9 +12,9 @@ declare global {
     }
 
     interface HTMLElement {
-        appendHtml(htmlString: string): void;
         $<T extends HTMLElement>(selector: string): T | null;
         $$<T extends HTMLElement>(selector: string): NodeListOf<T>;
+        appendHtml(htmlString: string): void;
     }
 
     interface Number {
@@ -25,9 +25,9 @@ declare global {
         multFloor(multiplier: number): number;
         multRound(multiplier: number): number;
 
-        toFeetInches(decimals?: number, unit?: LengthUnit): string;
-        asInches(unit?: LengthUnit): number;
-        asCm(unit?: LengthUnit): number;
+        toFeetInches(decimals?: number, fromUnit?: LengthUnit): string;
+        asInches(fromUnit?: LengthUnit): number;
+        asCm(fromUnit?: LengthUnit): number;
     }
 
     function loaded(): Promise<void>;
@@ -40,21 +40,27 @@ Object.defineProperty(NodeList.prototype, 'toArray', { value: function() { retur
 
 Object.defineProperty(HTMLCollection.prototype, 'toArray', { value: function() { return [...this]; } });
 
-Object.defineProperty(HTMLElement.prototype, 'appendHtml', { value: function(this: HTMLElement, htmlString: string) { this.insertAdjacentHTML('beforeend', htmlString); } });
-
 Object.defineProperty(HTMLElement.prototype, '$', { value: function(this: HTMLElement, selector: string) { return this.querySelector(selector); } });
 Object.defineProperty(HTMLElement.prototype, '$$', { value: function(this: HTMLElement, selector: string) { return this.querySelectorAll(selector); } });
 
-Object.defineProperty(Number.prototype, 'roundTo', { value: function(this: number, precision = 0) { return Number(this.toFixed(precision)) } });
-Object.defineProperty(Number.prototype, 'floorTo', { value: function(this: number, precision = 0) { return Math.floor(this * (10 ** precision)) / (10 ** precision) } });
+Object.defineProperty(HTMLElement.prototype, 'appendHtml', { 
+    value: function(this: HTMLElement, htmlString: string) { this.insertAdjacentHTML('beforeend', htmlString); } 
+});
+
+Object.defineProperty(Number.prototype, 'roundTo', { 
+    value: function(this: number, precision = 0) { return Number(this.toFixed(precision)) } 
+});
+Object.defineProperty(Number.prototype, 'floorTo', { 
+    value: function(this: number, precision = 0) { return Math.floor(this * (10 ** precision)) / (10 ** precision) } 
+});
 
 Object.defineProperty(Number.prototype, 'mult', { value: function(this: number, multiplier: number) { return this * multiplier } });
 Object.defineProperty(Number.prototype, 'multFloor', { value: function(this: number, multiplier: number) { return Math.floor(this * multiplier) } });
 Object.defineProperty(Number.prototype, 'multRound', { value: function(this: number, multiplier: number) { return Math.round(this * multiplier) } });
 
 Object.defineProperty(Number.prototype, 'toFeetInches', { 
-    value: function(this: number, decimals: number = 0, unit: LengthUnit = "inches") { 
-        const totalInches = this.asInches(unit);
+    value: function(this: number, decimals: number = 0, fromUnit: LengthUnit = "inches") { 
+        const totalInches = this.asInches(fromUnit);
         const feet = Math.floor(totalInches / 12);
         const inches = (totalInches - feet * 12).roundTo(decimals);
         return `${feet}'${inches}"`;
@@ -62,14 +68,14 @@ Object.defineProperty(Number.prototype, 'toFeetInches', {
 });
 
 Object.defineProperty(Number.prototype, 'asInches', { 
-    value: function(this: number, unit: LengthUnit = "centimeters") { 
-        return unit === "centimeters" ? this / 2.54 : this;
+    value: function(this: number, fromUnit: LengthUnit = "centimeters") { 
+        return fromUnit === "centimeters" ? this / 2.54 : this;
     } 
 });
 
 Object.defineProperty(Number.prototype, 'asCm', { 
-    value: function(this: number, unit: LengthUnit = "inches") { 
-        return unit === "inches" ? this * 2.54 : this;
+    value: function(this: number, fromUnit: LengthUnit = "inches") { 
+        return fromUnit === "inches" ? this * 2.54 : this;
     } 
 });
 
