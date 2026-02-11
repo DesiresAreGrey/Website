a9b9d14c4f41a219938aa9517773dda439001f3b
DesiresAreGrey <desiresaregrey@gmail.com>
2026-02-11 14:34:42 -0500

    Update updateapexcard.ts

--- src/misc/updateapexcard.ts
+++ src/misc/updateapexcard.ts
@@ -2,17 +2,21 @@ import { Utils } from "../utils/utils.js";
 
 const cached = localStorage.getItem("apex-season-banner")?.parseJson();
 const card = document.querySelector("#apex-weapon-stats-card") as HTMLElement;
-console.log(cached.url);
 
-if (card && cached?.url) {
-    card.style.setProperty(`--image`, `url('${cached.url}')`);
-    console.log(card.style);
+if (cached?.url) {
+    card?.style.setProperty(`--image`, `url('${cached.url}')`);
     
     // 86400000 is 24hours
     if (Date.now() - cached.time > 86400000) {
-        console.log(Date.now() - cached.time);
-        const imageUrl = (await Utils.fetchJson("https://desiresapi.vercel.app/api/apex/season-banner"))?.url;
-        card.style.setProperty(`--image`, `url('${imageUrl}')`);
-        localStorage.setItem("apex-season-banner", ({ time: Date.now(), url: imageUrl }).toJson());
+        setImage();
     }
+}
+else {
+    setImage();
+}
+
+async function setImage() {
+    const imageUrl = (await Utils.fetchJson("https://desiresapi.vercel.app/api/apex/season-banner"))?.url;
+    card?.style.setProperty(`--image`, `url('${imageUrl}')`);
+    localStorage.setItem("apex-season-banner", ({ time: Date.now(), url: imageUrl }).toJson());
 }
