0dbf5fa86596aafac451e7d9463cad49b863a96a
DesiresAreGrey <desiresaregrey@gmail.com>
2026-02-12 01:05:43 -0500

    minecraft banner

--- docs/misc/index.md
+++ docs/misc/index.md
@@ -16,7 +16,7 @@ h1 {
       <h2>Apex Legends Weapon Stats</h2>
       <p>Compare detailed weapon stats for Apex Legends weapons across across seasons, firing modes, and weapon variants.</p>
     </a>
-    <a class="card" href="modded-minecraft-versions" style="--image: url('https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/MCV_HOL25Drop_MoM_DotNet_Homepage_2560x932.jpg'); --scale: 1.1;">
+    <a class="card" id="modded-minecraft-versions-card" href="modded-minecraft-versions" style="--image: url('/assets/misc/minecraft/cover.png'); --scale: 1.1;">
       <h2>Popular Modded Minecraft Versions</h2>
       <p>Charts showing the number of mods for different Minecraft versions</p>
     </a>
--- src/misc/updatemisccards.ts
+++ src/misc/updatemisccards.ts
@@ -1,27 +1,62 @@
-import { Utils } from "../utils/utils.js";
 import { API } from "../utils/api.js";
+import { Utils } from "../utils/utils.js";
 
-const cached = localStorage.getItem("apex-season-banner")?.parseJson();
-const card = document.querySelector("#apex-weapon-stats-card") as HTMLElement;
+{
+    const cached = localStorage.getItem("apex-season-banner")?.parseJson();
+    const card = document.querySelector("#apex-weapon-stats-card") as HTMLElement;
 
-if (cached?.url) {
-    card?.style.setProperty(`--image`, `url('${cached.url}')`);
-    
-    // 86400000 is 24hours
-    if (Date.now() - cached.time > 86400000) {
-        console.log("Apex season banner cache expired, fetching new banner");
+    if (cached?.url) {
+        card?.style.setProperty(`--image`, `url('${cached.url}')`);
+        
+        // 86400000 is 24hours
+        if (Date.now() - cached.time > 86400000) {
+            console.log("Apex season banner cache expired, fetching new banner");
+            setImage();
+        }
+        else
+            console.log("Using cached Apex season banner");
+    }
+    else {
+        console.log("No cached Apex season banner found, fetching new banner");
         setImage();
     }
-    else
-        console.log("Using cached Apex season banner");
-}
-else {
-    console.log("No cached Apex season banner found, fetching new banner");
-    setImage();
+
+    async function setImage() {
+        const imageUrl = (await API.fetchJson("misc/apex/season-banner"))?.url;
+        card?.style.setProperty(`--image`, `url('${imageUrl}')`);
+        localStorage.setItem("apex-season-banner", ({ time: Date.now(), url: imageUrl }).toJson());
+    }
 }
 
-async function setImage() {
-    const imageUrl = (await API.fetchJson("misc/apex/season-banner"))?.url;
-    card?.style.setProperty(`--image`, `url('${imageUrl}')`);
-    localStorage.setItem("apex-season-banner", ({ time: Date.now(), url: imageUrl }).toJson());
+{
+    const cached = localStorage.getItem("minecraft-update-banner")?.parseJson();
+    const card = document.querySelector("#modded-minecraft-versions-card") as HTMLElement;
+
+    if (cached?.url) {
+        card?.style.setProperty(`--image`, `url('${cached.url}')`);
+        
+        // 86400000 is 24hours
+        if (Date.now() - cached.time > 86400000) {
+            console.log("Minecraft banner cache expired, fetching new banner");
+            setImage();
+        }
+        else
+            console.log("Using cached Minecraft banner");
+    }
+    else {
+        console.log("No cached Minecraft banner found, fetching new banner");
+        setImage();
+    }
+
+    async function setImage() {
+        const latest = (await Utils.fetchJson("https://launchercontent.mojang.com/v2/games.json")).entries.find((entry: any) => entry.productId === "java");
+        
+
+        let imageUrl = latest.heroImage.url;
+        if (imageUrl.startsWith("/"))
+            imageUrl = `https://launchercontent.mojang.com${imageUrl}`;
+        console.log(imageUrl);
+        card?.style.setProperty(`--image`, `url('${imageUrl}')`);
+        localStorage.setItem("minecraft-update-banner", ({ time: Date.now(), url: imageUrl }).toJson());
+    }
 }
