0b712348aba6344cc0bc1b77cfeb734fed90298b
DesiresAreGrey <desiresaregrey@gmail.com>
2026-02-12 15:05:06 -0500

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,30 +1,39 @@
 import { JsonClient } from "./jsonclient.js";
 
 export class API {
-    static async getUrl(): Promise<string> {
-        return "https://desiresapi.runasp.net/";
+    static #urlPromise: Promise<string> | null = null;
+
+    static get url(): Promise<string> {
+        this.#urlPromise = Promise.resolve("https://desiresapi.runasp.net/");
+        return this.#urlPromise;
+
         // for if i pay for it
-        try {
-            const response = await fetch("https://api.desiresaregrey.com/", { 
-                method: 'HEAD', 
-                mode: 'no-cors' 
-            });
-            return response.ok || response.type === 'opaque' ? "https://api.desiresaregrey.com/" : "https://desiresapi.runasp.net/";
-        } 
-        catch (error) {
-            return "https://desiresapi.runasp.net/";
-        }
+        this.#urlPromise ??= (async () => {
+            try {
+                const response = await fetch("https://api.desiresaregrey.com/", { 
+                    method: 'HEAD', 
+                    mode: 'no-cors' 
+                });
+                return response.ok || response.type === 'opaque' ? "https://api.desiresaregrey.com/" : "https://desiresapi.runasp.net/";
+            } 
+            catch (error) {
+                return "https://desiresapi.runasp.net/";
+            }
+        })();
+        return this.#urlPromise!;
     }
 
     static async get<T = any>(endpoint: string): Promise<T> {
         if (endpoint.startsWith('/'))
             endpoint = endpoint.substring(1);
-        return JsonClient.get(await API.getUrl() + endpoint);
+        return JsonClient.get(await API.url + endpoint);
     }
 
     static async post<T = any>(endpoint: string, data: any): Promise<T> {
         if (endpoint.startsWith('/'))
             endpoint = endpoint.substring(1);
-        return JsonClient.post(await API.getUrl() + endpoint, data);
+        return JsonClient.post(await API.url + endpoint, data);
     }
 }
+
+void API.url;
